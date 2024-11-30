2673c5d96aac9cf78919b387232e051a6c5bb915
AhmedTrigui137 <123263433+AhmedTrigui137@users.noreply.github.com>
2024-11-30 19:33:11 +0100

    hsgd

---
+++ streaming.py
@@ -0,0 +1,110 @@
+import json
+import time
+import psycopg2
+from confluent_kafka import SerializingProducer
+
+import tools.hiInterns as hiInterns
+import tools.keejob as keejob
+import tools.farojob as farojob
+
+
+# connection = psycopg2.connect(
+#     host="localhost",
+#     database="job_board",
+#     user="postgres",
+#     password="postgres",
+#     port="6543",
+# )
+
+
+def delivery_report(err, msg):
+    if err is not None:
+        print(f"Failed to deliver message: {err}")
+    else:
+        print(f"Message delivered to {msg.topic}: {msg.partition()}")
+
+
+def hiInterns_old_links():
+    # cursor = connection.cursor()
+    # cursor.execute("SELECT link FROM hi_interns")
+    # old_links = cursor.fetchall()
+    # cursor.close()
+    # return [link[0] for link in old_links]
+    return []
+
+
+def keejob_old_links():
+    # cursor = connection.cursor()
+    # cursor.execute("SELECT link FROM keejob")
+    # old_links = cursor.fetchall()
+    # cursor.close()
+    # return [link[0] for link in old_links]
+    return []
+
+
+def farojob_old_links():
+    # cursor = connection.cursor()
+    # cursor.execute("SELECT link FROM farojob")
+    # old_links = cursor.fetchall()
+    # cursor.close()
+    # return [link[0] for link in old_links]
+    return []
+
+
+def main():
+    hiInters_latest_post = "None"
+    keejob_latest_post = "None"
+    farojob_latest_post = "None"
+    hiInterns_old_links_ = hiInterns_old_links()
+    keejob_old_links_ = keejob_old_links()
+    farojob_old_links_ = farojob_old_links()
+    producer = SerializingProducer({"bootstrap.servers": "localhost:9092"})
+
+    while True:
+        # hiInterns_post = hiInterns.get_latest_post()
+        # if hiInters_latest_post != hiInterns_post:
+        #     hiInters_latest_post = hiInterns_post
+        #     hiInterns_data, hiInterns_old_links_ = hiInterns.get_all_data(hiInterns_old_links_)
+        #     for link, body in hiInterns_data.items():
+        #         producer.produce(
+        #             "hi_interns",
+        #             key=link,
+        #             value=json.dumps({"link": link, "body": str(body)}),
+        #             on_delivery=delivery_report,
+        #         )
+        #         producer.poll(0)
+        #         time.sleep(1)
+
+        # keejob_post = keejob.get_latest_post()
+        # if keejob_latest_post != keejob_post:
+        #     keejob_latest_post = keejob_post
+        #     keejob_data, keejob_old_links_ = keejob.get_all_data(keejob_old_links_)
+        #     for link, body in keejob_data.items():
+        #         producer.produce(
+        #             "keejob",
+        #             key=link,
+        #             value=json.dumps({"link": link, "body": str(body)}),
+        #             on_delivery=delivery_report,
+        #         )
+        #         producer.poll(0)
+        #         time.sleep(1)
+
+        farojob_post = farojob.get_latest_post()
+        if farojob_latest_post != farojob_post:
+            farojob_latest_post = farojob_post
+            farojob_data, farojob_old_links_ = farojob.get_all_data(farojob_old_links_)
+            for link, body in farojob_data.items():
+                producer.produce(
+                    "farojob",
+                    key=link,
+                    value=json.dumps({"link": link, "body": str(body[0]), "date": body[1]}),
+                    on_delivery=delivery_report,
+                )
+                producer.poll(0)
+                time.sleep(1)
+
+        time.sleep(30)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tools/keejob.py
@@ -0,0 +1,86 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+
+
+from .tools import convert_date
+
+url = "https://www.keejob.com"
+
+
+def get_number_of_pages():
+    response = requests.get(url + "/offres-emploi/")
+    soup = BeautifulSoup(response.text, "html.parser")
+    pages = soup.find_all("li", class_="page-item")
+    page_number = pages[3].find("a")["aria-label"].split()[-1]
+    return int(page_number)
+
+
+def get_latest_post():
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    post = soup.select_one(
+        "div.block_white_a.post.clearfix.silver-job-block > div.content.row-fluid > div.span8"
+    )
+    return post
+
+
+def get_posts_links(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    posts = soup.select(
+        "div.block_white_a.post.clearfix.silver-job-block > div.content.row-fluid > div.span8"
+    )
+    links = [post.find("a")["href"] for post in posts if post.find("a")]
+    return links
+
+
+def get_post_content(link):
+    request = requests.get(link)
+    soup = BeautifulSoup(request.text, "html.parser")
+    content = soup.find("div", {"id": "main", "class": "span12 page image-preloader"})
+
+    return content
+
+
+def get_all_data(old_links=[]):
+    links = []
+    data = {}
+    n = get_number_of_pages()
+    # for i in range(1, n + 1):
+    for i in range(1, 4):
+        page_url = f"{url}/offres-emploi/?page/{i}"
+        links += get_posts_links(page_url)
+        for link in links:
+            if link in old_links:
+                return data, old_links
+            data[link] = get_post_content(url + link)
+            old_links.insert(0, link)
+    return data, old_links
+
+
+def get_post_details(content):
+    details = {}
+    block = content.find("div", {"class": "span9 content"}).text
+    text = re.sub(r"\t+", "", block)
+    text = text.strip().split("\n")
+    text = [line for line in text if line != ""]
+    details["company"] = text[0]
+    details["job_title"] = content.find("h2", class_="job-title").text
+    details["description"] = content.find(
+        "div", class_="block_a span12 no-margin-left"
+    ).text
+    items = content.find_all("div", class_="meta")
+    data = {}
+    for meta in items:
+        label = meta.find("b").text.strip().replace(":", "")
+        value = meta.find("br").next_sibling.strip() if meta.find("br") else ""
+        value = re.sub(r"[\s\n\t]+", " ", value).strip()
+        data[label] = value
+    details["expiration_date"] = convert_date(data["Publiée le"])
+    details["workplace"] = data["Lieu de travail"]
+    for key, value in data.items():
+        if key not in ["Publiée le", "Lieu de travail"]:
+            details["description"] += f"\n{key}: {value}"
+
+    return details
